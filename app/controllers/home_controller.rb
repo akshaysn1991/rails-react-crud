5f6b37533fedf24abbfafc49620e0190ba02b1f2
akshay-powerschool <akshaya.sn@powerschool.com>
2020-09-01 14:44:13 +0530

    Code refactor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,12 @@
 class HomeController < ApplicationController
     def index
-
     end
 
     def import
     	if params[:file].blank?
     		redirect_to root_path, notice: "Select CSV file first and Import"
     	else
-		    	Question.my_import(params[:file])
+		    Question.my_import(params[:file])
 		    redirect_to home_view_data_path, notice: "Successfully Imported Data!!!"
 		end
 	end
